c6c73afdf3001f76fbf91ecea5dbff9fa7d513a9
Nick Stone <stone.nicholas@gmail.com>
2017-05-09 19:46:20 -0400

    the application now asks you to choose a file to work with on startup. Eventually this will be where the video is selected, for now it allows you to choose your own placeholder image

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,41 @@
 extern crate gtk;
+
 use gtk::prelude::*;
+
 mod click_object;
 use click_object::{Control,LabelFrame,Attachable};
 
-use gtk::{Button, Window, WindowType, Image,Orientation,Box,ScrolledWindow,Paned};
+use std::path::PathBuf;
+
+use gtk::{Button, WindowType, Image,Orientation,Box,ScrolledWindow,Paned,FileChooserDialog,DialogExt,FileChooserAction,ResponseType,Window};
 
 fn main() {
     if gtk::init().is_err() {
         println!("Failed to initialize GTK.");
         return;
     }
-    
+//Select file to open
+	 let chooser_window=Window::new(WindowType::Toplevel);
+	 let chooser=FileChooserDialog::new::<Window>(Some("Open Video"),Some(&chooser_window),FileChooserAction::Open);
+	 chooser.add_button("Open",ResponseType::Ok.into());
+	 chooser.add_button("Cancel",ResponseType::Cancel.into());
+	 let filename:PathBuf;
+	 let response_code=chooser.run();
+		if response_code==ResponseType::Ok.into(){
+				//We've got a file, let's open it
+				 filename=chooser.get_filename().unwrap();
+				 chooser.destroy();
+		}else if response_code == ResponseType::Cancel.into(){
+		      	 //if they don't want to open a file, what are we doing
+				     println!("No file, no clickbait");
+				     return;
+			} else {
+			//eeek
+		     println!("Unknown signal from open dialog");
+		     return;
+		}
+
+
     let window = Window::new(WindowType::Toplevel);
     window.set_title("clickbait");
     window.set_default_size(350, 70);
@@ -18,7 +43,7 @@ fn main() {
     let rew_button = Button::new_with_label("Rewind");
     let ff_button = Button::new_with_label("Fast Forward");
     
-    let image=Image::new_from_file("/home/nstone/Desktop/Advisor email.png");
+    let image=Image::new_from_file(filename);
     let top_box=Paned::new(Orientation::Horizontal);
     let my_box=Paned::new(Orientation::Vertical);
     let play_box=Box::new(Orientation::Horizontal,10);
